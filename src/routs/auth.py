53427d1ccab3a14c987a60a6272b4485251389b3
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-06-06 10:57:25 +0500

    translate pages

--- src/routs/auth.py
+++ src/routs/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from models.users import User
@@ -45,5 +45,7 @@ def signup_post():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'logout'
+    logout_user()
+    return redirect(url_for('start_page.index'))
